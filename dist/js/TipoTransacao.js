e83f8bbca5fc6b72684614a841f116ce859c8397
Lucas Mendes <lms252006@gmail.com>
2025-06-19 13:26:29 -0300

    delete pasta js

--- dist/js/TipoTransacao.js
+++
@@ -1,6 +0,0 @@
-var TipoTransação;
-(function (TipoTransação) {
-    TipoTransação["DEPOSITO"] = "Dep\u00F3sito";
-    TipoTransação["TRANSFERENCIA"] = "Transfer\u00EAncia";
-    TipoTransação["PAGAMENTO_BOLETO"] = "Pagamento de Boleto";
-})(TipoTransação || (TipoTransação = {}));
